16627afd925aa83f4833b8b419099667831fdba9
Libbi <171863901+Libbilikestocode@users.noreply.github.com>
2026-06-13 21:13:10 -0400

    Complete train_random_forest run.py .

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -71,10 +71,9 @@ def go(args):
     # Then fit it to the X_train, y_train data
     logger.info("Fitting")
 
-    ######################################
+    ############################# INSERT CODE HERE #########
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
-    ######################################
+    sk_pipe.fit(X_train, y_train)
 
     # Compute r2 and MAE
     logger.info("Scoring")
@@ -96,7 +95,9 @@ def go(args):
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_model=sk_pipe,
+        path="random_forest_dir",
+# YOUR CODE HERE
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -120,6 +121,7 @@ def go(args):
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
     # YOUR CODE HERE
+    run.summary["mae"] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -162,9 +164,10 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
+    SimpleImputer(strategy="most_frequent"),
+    OneHotEncoder()
     )
-    ######################################
+    ############### CODE HERE #######################
 
     # Let's impute the numerical columns to make sure we can handle missing values
     # (note that we do not scale because the RF algorithm does not need that)
@@ -224,8 +227,9 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # HINT: Use the explicit Pipeline constructor so you can assign the names to the steps, do not use make_pipeline
 
     sk_pipe = Pipeline(
-        steps =[
-        # YOUR CODE HERE
+        steps =[ ("preprocessor", preprocessor),
+                 ("random_forest", random_forest)
+# YOUR CODE HERE
         ]
     )
 
